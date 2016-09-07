0732df8ae47dd763f0a96fd7db9ee88a25e2e997
mistvan22 <pocokxtrame@gmail.com>
2016-09-07 11:39:36 +0200

    Főoldal kereső box frontend v1

--- wp-content/themes/Avada-Child-Theme/includes/shortcodes/SearchBoxModule.php
+++ wp-content/themes/Avada-Child-Theme/includes/shortcodes/SearchBoxModule.php
@@ -46,7 +46,28 @@ class Search_Box_Module extends AvadaTemplate
         $r->_('<div class="'.self::SHORTCODE_TAG.'">');
 
             $r->_($r->row_start());
-              $r->_('asdasd');
+                // Fejrész
+                $r->_('<div class="sb-header">Ingatlankeresés</div>');
+
+                // Body
+                $r->_('<div class="sb-body">');
+                    // Régió
+                    $r->_($r->column(1, 3, '<div class="sb-inp-wrapper"><label for="sb-inp-regio">Régió</label><select name="region" id="sb-inp-regio">'.$regio_options.'</select></div>', 'no'));
+                    // Szobaszám
+                    $r->_($r->column(1, 6, '<div class="sb-inp-wrapper"><label for="sb-inp-szobaszam">Szobák száma</label><select name="rooms" id="sb-inp-szobaszam">'.$szoba_options.'</select></div>', 'no'));
+                    // Alapterület
+                    $r->_($r->column(1, 6, '<div class="sb-inp-wrapper"><label for="sb-inp-alapnm">Alapterület</label><input type="number" min="0" step="1" value="" placeholder="nm" id="sb-inp-alapnm"></div>', 'no'));
+                    // Kategória
+                    $r->_($r->column(1, 3, '<div class="sb-inp-wrapper"><label for="sb-inp-kategoria">Kategória</label><select name="category" id="sb-inp-kategoria">'.$kategoria_options.'</select></div>', 'no'));
+                    $r->_('<div class="fusion-clearfix"></div>');
+                $r->_('</div>');
+
+                // Lábrész
+                $r->_('<div class="sb-footer">');
+                    //$r->_($r->column(1, 6, '<a href="">Térképes kereső</a>'));
+                    $r->_($r->column(1, 2, '<a href="">Részletesebben szeretnék keresni <i class="fa fa-caret-right"></i></a>', 'no', true));
+                    $r->_('<div class="fusion-clearfix"></div>');
+                $r->_('</div>');
             $r->_($r->row_end());
 
         $r->_('</div>');
