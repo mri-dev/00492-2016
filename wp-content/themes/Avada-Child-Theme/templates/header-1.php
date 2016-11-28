b4d82006999ce5488a2d81fbed9b2d0fa4d72f5c
mistvan22 <pocokxtrame@gmail.com>
2016-11-28 14:58:19 +0100

    Save

--- wp-content/themes/Avada-Child-Theme/templates/header-1.php
+++ wp-content/themes/Avada-Child-Theme/templates/header-1.php
@@ -1,8 +1,13 @@
 <div class="fusion-header-sticky-height"></div>
 <div class="fusion-header">
     <div class="fusion-row">
-        <?php avada_logo(); ?>
-        <?php avada_main_menu(); ?>
-        <?php echo get_template_part('templates/header-search'); ?>
+        <div class="header-holder">
+          <?php avada_logo(); ?>
+          <div class="side-of-logo">
+            <?php get_template_part('templates/header-search'); ?>
+            <div class="clearfix"></div>
+            <?php avada_main_menu(); ?>
+          </div>
+        </div>
     </div>
 </div>
